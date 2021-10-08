0bdac129130524f26b6099f27f95103ba16cb5cc
Anastasia Maryina <nastiacool55@gmail.com>
2021-10-08 19:15:05 +0300

    added filtering of employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,11 @@ class App extends Component {
         {name: 'John Doe', salary: 800, bonus: false, raise: false, id: 2},
         {name: 'Ivan Ivanov', salary: 500, bonus: true, raise: false, id: 3}
       ],
-      searchValue: ''
+      searchValue: '',
+      filter: {
+        raise: false,
+        salary: false
+      }
     }
     this._nextId = this.state.data.length + 1;
   }
@@ -121,12 +125,44 @@ class App extends Component {
     this.setState({searchValue});
   }
 
+  onToggleFilter = (filterType) => {
+    this.setState(({filter}) => {
+      return {
+        filter: {...filter, [filterType]: !filter[filterType]}
+      }
+    });
+  }
+
+  onClearFilters = () => {
+    this.setState({
+      filter: {
+        raise: false,
+        salary: false
+      }
+    });
+  }
+
+  filterEmployees = (data) => {
+    const {filter} = this.state;
+    if (filter.raise && filter.salary) {
+      return data.filter(employee => employee.raise === true && employee.salary > 1000)
+    }
+    if (filter.raise) {
+      return data.filter(employee => employee.raise === true);
+    }
+    if (filter.salary) {
+      return data.filter(employee => employee.salary > 1000);
+    }
+    return data;
+  }
+
   render() {
     const {data, searchValue} = this.state;
     const totalEmployees = data.length;
     const totalEmployeesToBeRewarded = data.filter(employee => employee.bonus === true).length;
 
-    const searchResult = this.searchEmployee(searchValue, data);
+    let visibleEmployees = this.searchEmployee(searchValue, data);
+    visibleEmployees = this.filterEmployees(visibleEmployees);
 
     return (
       <div className="app">
@@ -137,11 +173,14 @@ class App extends Component {
   
         <div className="wrapper-block">
           <EmployeesSearch onUpdateSearch={this.onUpdateSearch}/>
-          <EmployeesFilter />
+          <EmployeesFilter 
+            onToggleFilter={this.onToggleFilter}
+            onClearFilters={this.onClearFilters}
+          />
         </div>
   
         <EmployeesList 
-          data={searchResult}
+          data={visibleEmployees}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
--- src/components/employees-filter/employees-filter.js
+++ src/components/employees-filter/employees-filter.js
@@ -1,25 +1,90 @@
-const EmployeesFilter = () => {
-    return (
-        <div className="btn-group mt-3">
-            <button 
-                className="btn btn-light"
-                type="button">
-                    All Employees
-            </button>
+import { Component } from 'react';
 
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Employees to Raise
-            </button>
+class EmployeesFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            raise: false,
+            salary: false
+        };
+    }
 
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Employees with $1000 (and more) month salary
-            </button>
-        </div>
-    );
+    onToggleFilter = (prop) => {
+        this.setState((state) => ({
+            [prop]:  !state[prop]
+        }));
+        const {onToggleFilter} = this.props;
+        onToggleFilter(prop);
+    }
+
+    onClearFilters = () => {
+        // Clear filters in root app component
+        const {onClearFilters} = this.props;
+        onClearFilters();
+        // Clear filters locally
+        this.setState({
+            raise: false,
+            salary: false
+        });
+    }
+
+    toggleBtnClasses = (buttonFilter) => {
+        const clickedBtnClasses = "btn btn-light";
+        const unclickedBtnClasses = "btn btn-outline-light";
+        const {raise, salary} = this.state;
+        
+        switch (buttonFilter) {
+            case "no-filter": 
+                if (!raise && !salary) {
+                    return clickedBtnClasses;
+                } else {
+                    return unclickedBtnClasses;
+                }
+            case "raise":
+                if (raise) {
+                    return clickedBtnClasses;
+                } else {
+                    return unclickedBtnClasses
+                }
+            case "salary":
+                if (salary) {
+                    return clickedBtnClasses;
+                } else {
+                    return unclickedBtnClasses;
+                }
+            default: return;
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="btn-group mt-3">
+                <button 
+                    className={this.toggleBtnClasses("no-filter")}
+                    type="button"
+                    onClick={this.onClearFilters}>
+                        All Employees
+                </button>
+    
+                <button 
+                    className={this.toggleBtnClasses("raise")}
+                    type="button"
+                    data-filter="raise"
+                    onClick={(e) => this.onToggleFilter(e.currentTarget.dataset.filter)}>
+                        Employees to Raise
+                </button>
+    
+                <button 
+                    className={this.toggleBtnClasses("salary")}
+                    type="button"
+                    data-filter="salary"
+                    onClick={(e) => this.onToggleFilter(e.currentTarget.dataset.filter)}>
+                        Employees with $1000 (and more) month salary
+                </button>
+            </div>
+        );
+    }
 };
 
 export default EmployeesFilter;
